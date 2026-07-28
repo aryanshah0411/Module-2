a146590245f8dc609bb12f28b6fa8372005c1d33
aryanshah0411 <aryanshah0411@gmail.com>
2026-07-28 18:50:58 +0530

    activities

--- M2- HomeWork/age_checker_hw.py
+++
@@ -1,6 +0,0 @@
-age = int(input("Enter your age: "))
-if age >= 10:
-    if age <= 20:
-        print("Your age is between 10 and 20 years old.")
-else:
-    print("Yout age is not between 10 and 20 years old.")
---
+++ lesson2/activity2.py
@@ -0,0 +1,9 @@
+n = 11
+for i in range (1,11,1):
+    print(i)
+print("Loop2")
+for i in range (10):
+    print(i)
+print("Loop3")
+for i in range (0,11,2):
+    print(i)
