b8b5830b2b87960e59a278d45dbbdc340fe87be5
aryanshah0411 <aryanshah0411@gmail.com>
2026-07-28 15:32:49 +0530

    age checker

---
+++ M2- HomeWork/age_checker.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+if age >= 10:
+    if age <= 20:
+        print("Your age is between 10 and 20 years old.")
+else:
+    print("Yout age is not between 10 and 20 years old.")
+    
