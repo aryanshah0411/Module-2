68d26aa7ba03e59de9852d1102822434c29989ce
aryanshah0411 <aryanshah0411@gmail.com>
2026-08-02 17:08:56 +0530

    whileloop

---
+++ lesson4/activity1.py
@@ -0,0 +1,9 @@
+i = 1
+while i <=5:
+    print("Putting ",i,"litre water")
+    j = 1
+    while j <= 2:
+        print("Adding ",j,"lemon ",)
+        j = j + 1
+    print()
+    i = i + 1
---
+++ lesson4/activity2.py
@@ -0,0 +1,9 @@
+i = 1
+while i <= 5:
+    print("Value of i",i)
+    j = 1
+    while j <= 5:
+        print(j, end = (" "))
+        j = j + 1
+    print()
+    i = i + 1
